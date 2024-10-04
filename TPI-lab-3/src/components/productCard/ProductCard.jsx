90bb38c8c8ec2dbcc233539088d4c15cf3b37a52
Sergio Duarte <romanduarte17@gmail.com>
2024-10-04 13:37:36 -0300

    buttons

--- TPI-lab-3/src/components/productCard/ProductCard.jsx
+++ TPI-lab-3/src/components/productCard/ProductCard.jsx
@@ -1,17 +1,24 @@
-import { Card } from "react-bootstrap";
-import './productCard.css'
+import { Card, Button } from "react-bootstrap";
+import './productCard.css';
 
+const ProductCard = ({ productName = "Unnamed Product", productBrand = "Unknown Brand", productPrice = "0.00", productImage = "" }) => {
+    const placeholderImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmNILEZppKJCs1LHgBaUGbbFzQJsv6b5bt-w&s";
 
-const ProductCard = ({ productName, productBrand, productPrice, productImage }) => {
     return (
         <Card className="card-container">
-            <Card.Img className="card-image" src={productImage !== "" ? productImage : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmNILEZppKJCs1LHgBaUGbbFzQJsv6b5bt-w&s"} />
+            <Card.Img
+                className="card-image"
+                src={productImage !== "" ? productImage : placeholderImage}
+                alt={productName}
+            />
             <Card.Body>
                 <Card.Subtitle>{productName}</Card.Subtitle>
                 <Card.Title>${productPrice}</Card.Title>
                 <Card.Text>{productBrand}</Card.Text>
+                <Button variant="success">Añadir</Button> {/* Botón de Bootstrap */}
             </Card.Body>
         </Card>
     );
 };
+
 export default ProductCard;
