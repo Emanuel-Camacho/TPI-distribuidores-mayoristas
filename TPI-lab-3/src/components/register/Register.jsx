87740adb9858fc51ffe691487e2dba8ecaf028f0
TommyGun1510 <bytomas135@gmail.com>
2024-11-02 15:56:57 -0300

    se agrega el logo a login y register

--- TPI-lab-3/src/components/register/Register.jsx
+++ TPI-lab-3/src/components/register/Register.jsx
@@ -3,7 +3,8 @@ import { Button, Card, Col, Form, FormGroup, Row } from
     "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
-import '../login/login.css'
+import '../register/Register.css'
+/* import '../login/login.css' */
 
 const Register = ({ onRegister }) => {
     const [user, setUser] = useState("");
@@ -140,78 +141,89 @@ const Register = ({ onRegister }) => {
     }
 
     return (
-        <>
-            <Card className="mt-5 mx-3 p-3 px-5 shadow">
-                <Card.Body>
-                    <Form>
-                        {/* Usuario */}
-                        <FormGroup className="mb-4">
-                            <Form.Control
-                                ref={userRef}
-                                value={user}
-                                onChange={handleChangeUser}
-                                type="text"
-                                placeholder="Ingresar Usuario" />
-                            {errors.user &&
-                                <p className="text-danger">El usuario no debe ser vacío</p>
-                            }
-                        </FormGroup>
-                        {/* Email */}
-                        <FormGroup className="mb-4">
-                            <Form.Control
-                                ref={emailRef}
-                                value={email}
-                                onChange={handleChangeEmail}
-                                type="email"
-                                placeholder="Ingresar Email" />
-                            {errors.email &&
-                                <p className="text-danger">El email no debe ser vacío</p>
-                            }
-                        </FormGroup>
-                        {/* Password */}
-                        <FormGroup className="mb-4">
-                            <Form.Control
-                                ref={passwordRef}
-                                value={password}
-                                onChange={handleChangePassword}
-                                type="password"
-                                required
-                                placeholder="Ingresar contraseña"
-                            />
-                            {errors.password &&
-                                <p className="text-danger">El password no debe ser vacío</p>}
-                        </FormGroup>
-                        <FormGroup className="mb-4">
-                            <Form.Control
-                                ref={passwordRepeatRef}
-                                value={passwordRepeat}
-                                onChange={handleChangePasswordRepeat}
-                                type="password"
-                                required
-                                placeholder="Repetir contraseña"
-                            />
-                            {errors.passwordRepeat &&
-                                <p className="text-danger">Las contraseñas deben coincidir</p>}
-                        </FormGroup>
-                    </Form>
-                    <Row>
-                        <Col />
-                        <Col md={8} className="d-flex justify-content-end">
-                            <Button className="register-button" variant="secondary"
+        <Row className="align-items-center min-vh-100 mx-0">
+            <Col xs={12} md={5} className="text-center">
+                <a href="/">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1THseam-yBpdGJYlt60zc_zFR-vlRSHuiRw&s" alt="logo" className="logo-img mb-2" />
+                </a>
+            </Col>
+            <Col xs={12} md={7} className="mx-auto">
+                <Card className="mt-5 mx-3 p-3 px-5 no-shadow">
+                    <Card.Body>
+                        <Form>
+                            {/* Usuario */}
+                            <FormGroup className="mb-4">
+                                <Form.Control
+                                    className="input"
+                                    ref={userRef}
+                                    value={user}
+                                    onChange={handleChangeUser}
+                                    type="text"
+                                    placeholder="Ingresar Usuario" />
+                                {errors.user &&
+                                    <p className="text-danger">El usuario no debe ser vacío</p>
+                                }
+                            </FormGroup>
+                            {/* Email */}
+                            <FormGroup className="mb-4">
+                                <Form.Control
+                                    className="input"
+                                    ref={emailRef}
+                                    value={email}
+                                    onChange={handleChangeEmail}
+                                    type="email"
+                                    placeholder="Ingresar Email" />
+                                {errors.email &&
+                                    <p className="text-danger">El email no debe ser vacío</p>
+                                }
+                            </FormGroup>
+                            {/* Password */}
+                            <FormGroup className="mb-4">
+                                <Form.Control
+                                    className="input"
+                                    ref={passwordRef}
+                                    value={password}
+                                    onChange={handleChangePassword}
+                                    type="password"
+                                    required
+                                    placeholder="Ingresar contraseña"
+                                />
+                                {errors.password &&
+                                    <p className="text-danger">El password no debe ser vacío</p>}
+                            </FormGroup>
+                            <FormGroup className="mb-4">
+                                <Form.Control
+                                    className="input"
+                                    ref={passwordRepeatRef}
+                                    value={passwordRepeat}
+                                    onChange={handleChangePasswordRepeat}
+                                    type="password"
+                                    required
+                                    placeholder="Repetir contraseña"
+                                />
+                                {errors.passwordRepeat &&
+                                    <p className="text-danger">Las contraseñas deben coincidir</p>}
+                            </FormGroup>
+                            <Button className="w-100 register-button mb-3" variant="secondary"
                                 onClick={handleLogin}
                                 type="button">
                                 Registrarse
                             </Button>
+                        </Form>
+                    </Card.Body>
+                    <Row className="center-text mt-3">
+                        <Col md="auto">
+                            <p className="mb-0">
+                                ¿Ya tienes cuenta?
+                                <a onClick={handleLoginRedirect} href="#"> Iniciar sesión</a>
+                            </p>
                         </Col>
                     </Row>
-                </Card.Body>
-            </Card>
-            <Row className="justify-content-end mt-3"> {/* no podemos alinear a la derecha */}
-                <Col md="auto" className="d-flex">
-                    <p className="mb-0">¿Ya tienes cuenta? <a onClick={handleLoginRedirect} href="#">Iniciar sesion</a></p>
-                </Col>
-            </Row>
-        </>
+
+                </Card>
+            </Col>
+
+        </Row>
     );
 };
 export default Register;
